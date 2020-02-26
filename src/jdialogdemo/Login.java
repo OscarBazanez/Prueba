b6b275d8b6859eed5d1d59cf23e1d11d66e0331c
OscarBazanez <41228346+OscarBazanez@users.noreply.github.com>
2020-02-25 20:32:32 -0600

    Update Login.java

--- src/jdialogdemo/Login.java
+++ src/jdialogdemo/Login.java
@@ -4,7 +4,6 @@ public class Login {
  
     public static boolean authenticate(String username, String password) {
         // hardcoded username and password
-     //,,,mñmjj
         if (username.equals("Ana") && password.equals("secretPassword")) {
             return true;
         }
